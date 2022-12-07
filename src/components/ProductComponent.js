df313d2c33a9b978685eaa21b9f9061929887089
Omole Daniel <omoledaniel13@gmail.com>
2022-12-07 11:44:03 +0100

    Adjusted product component

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -4,31 +4,34 @@ import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products);
-    const renderList = products.map((product) => {
-        return (
-            <div className='four column wide' key={product.id}>
-                <Link to={`/product/${product.id}`}>
-                    <div className='ui link cards'>
-                        <div className='card'>
-                            <div className='image'>
-                                <img src={product.image} alt={product.title}/>
-                            </div>
-                            <div className='content'>
-                                <div className='header'>
-                                    {product.title}
-                                </div>
-                                <div className='meta price'>£ {product.price}</div>
-                                <div className='meta'>{product.category}</div>
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-        );
-    })
     return (
-        <>{renderList}</>
-    )
+        <>
+            {
+                products.map((product) => {
+                    return (
+                        <div className='four column wide' key={product.id}>
+                            <Link to={`/product/${product.id}`}>
+                                <div className='ui link cards'>
+                                    <div className='card'>
+                                        <div className='image'>
+                                            <img src={product.image} alt={product.title}/>
+                                        </div>
+                                        <div className='content'>
+                                            <div className='header'>
+                                                {product.title}
+                                            </div>
+                                            <div className='meta price'>£ {product.price}</div>
+                                            <div className='meta'>{product.category}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    );
+                })
+            }
+        </>
+    );
 }
 
 export default ProductComponent;
